0c2889499d2ee62bad608dada1e87310d3b8f426
alex-dodoka <neksus.92@mail.ru>
2018-06-07 22:12:52 +0300

    Creating header with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 import {AppComponent} from './app.component';
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 
 
 @NgModule({
@@ -13,6 +20,10 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
